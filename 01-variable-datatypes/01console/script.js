cdc31ecb0515f186d0f16a2a2324e54c2445c99d
Janeth Duka <oloyaronaldjake@gmail.com>
2023-04-27 15:37:08 +0800

    humabol

---
+++ 01-variable-datatypes/01console/script.js
@@ -0,0 +1,28 @@
+// alert("Hello from Adele and Lionel");
+
+//log a number
+console.log(100);
+
+//log string
+console.log("Hello World");
+
+//log multiple values
+console.log(20, "hello", true);
+
+// Log a variable
+const x=100;
+console.log(x);
+
+//console error & warning
+console.error("Alert");
+console.warn("Warning")
+
+// log object as a table
+console.table({name: "Jake", email: "jake@gmail.com"});
+
+// Group console commands
+console.group('simple');
+console.log(x);
+console.error('Alert');
+console.warn('Warning');
+console.groupEnd();
