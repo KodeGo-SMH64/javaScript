35c202dfaf566a352d77e963b09feed41f9dd50e
Janeth Duka <oloyaronaldjake@gmail.com>
2023-04-28 09:11:13 +0800

    428 9 am

---
+++ 01-variable-datatypes/04datatypes/script.js
@@ -0,0 +1,26 @@
+//String
+const firstName = "Ryan";
+// console.log(firstName);
+
+// number
+const age = 30;
+const temp =98.9;
+
+//Boolean
+const hasKids = true;
+
+// Null
+const aptNumber = null;
+
+//Undefined
+// let score;
+// console.log(score);
+
+const score = undefined;
+
+//
+
+
+
+const output = score;;
+console.log(output, typeof output);
