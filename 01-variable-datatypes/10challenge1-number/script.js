741ef764276564fecc5c8a6fd253b31a6364fbdc
Janeth Duka <oloyaronaldjake@gmail.com>
2023-04-28 14:45:17 +0800

    afternoon 428

---
+++ 01-variable-datatypes/10challenge1-number/script.js
@@ -0,0 +1,5 @@
+const myString = 'developer';
+
+let myString = "developer";
+myNewString = myString.charAt(0).toUpperCase() + myString.slice(1);
+console.log(myNewString); 
---
+++ 01-variable-datatypes/13challenge2/script.js
@@ -0,0 +1,27 @@
+const x = Math.floor(Math.random() * 100) + 1;
+const y = Math.floor(Math.random() * 50) + 1;
+
+//SUM
+const sum = x + y;
+const sumOutput = `${x} + ${y} = ${sum}`;
+console.log(sumOutput);
+
+//DIFFERENCE
+const difference = x - y;
+const differenceOutput = `${x} + ${y} = ${difference}`;
+console.log(differenceOutput);
+
+//PRODUCT
+const product = x * y;
+const productOutput = `${x} * ${y} = ${product}`;
+console.log(productOutput);
+
+//QOUTIENT
+const quotient = x / y;
+const quotientOutput = `${x} / ${y} = ${quotient}`;
+console.log(quotientOutput);
+
+//REMAINDER
+const remainder = x % y;
+const remainderOutput = `${x} % ${y} = ${remainder}`;
+console.log(remainderOutput);
